883a40ac866edc21e6c86348e32f49de062c1eda
Peter Vasilyev <p.vasilyev@inbox.ru>
2021-12-10 18:52:48 +0300

    Add interactive segmentation widget

--- notebook_widgets.py
+++ notebook_widgets.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from ipywidgets import interact, Layout
+from ipywidgets import widgets
 from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
 from typing import List, Tuple
 from spine_segmentation import point_2_list, list_2_point, hash_point, \
-    Segmentation
+    Segmentation, segmentation_by_distance
 import meshplot as mp
 import random
 
@@ -64,7 +64,11 @@ def _show_cross_planes(ax, coord_1, coord_2, shape, color_1, color_2, border_col
 
 
 def show_3d_image(data: np.ndarray, cmap="gray"):
-    @interact(x=(0, data.shape[1] - 1), y=(0, data.shape[0] - 1), z=(0, data.shape[2] - 1), layout=Layout(width='500px'))
+    @widgets.interact(
+        x=widgets.IntSlider(min=0, max=data.shape[1] - 1, continuous_update=False),
+        y=widgets.IntSlider(min=0, max=data.shape[0] - 1, continuous_update=False),
+        z=widgets.IntSlider(min=0, max=data.shape[2] - 1, continuous_update=False),
+        layout=widgets.Layout(width='500px'))
     def display_slice(x, y, z):
         fig, ax = plt.subplots(2, 2, figsize=(15, 5),
                                gridspec_kw={
@@ -95,11 +99,40 @@ def show_3d_image(data: np.ndarray, cmap="gray"):
 
 
 def show_spines(spine_meshes: List[Polyhedron_3]):
-    @interact(index=(0, len(spine_meshes)), correct_spine=True)
+    @widgets.interact(index=(0, len(spine_meshes) - 1), correct_spine=True)
     def show_spine(index: int = 0, correct_spine: bool = True):
         print(index)
         show_3d_mesh(spine_meshes[index])
     return show_spine
+
+
+def interactive_segmentation(mesh: Polyhedron_3, correspondence,
+                             reverse_correspondence,
+                             skeleton_graph) -> widgets.Widget:
+    vertices, facets = _mesh_to_v_f(mesh)
+    slider = widgets.FloatSlider(min=0.0, max=1.0, step=0.01, value=0.75,
+                                 continuous_update=False)
+    plot = mp.plot(vertices, facets)
+
+    def do_segmentation(sensitivity=0.75):
+        segmentation = segmentation_by_distance(mesh, correspondence,
+                                                reverse_correspondence,
+                                                skeleton_graph, sensitivity)
+        plot.update_object(colors=_segmentation_to_colors(vertices, segmentation))
+
+        return segmentation
+
+    return widgets.interactive(do_segmentation, sensitivity=slider)
+
+
+def _segmentation_to_colors(vertices, segmentation: Segmentation):
+    colors = np.ndarray((vertices.shape[0], 3))
+    for i, vertex in enumerate(vertices):
+        if segmentation[hash_point(list_2_point(vertex))]:
+            colors[i] = RED
+        else:
+            colors[i] = GREEN
+    return colors
 
 
 def show_segmented_mesh(mesh: Polyhedron_3, segmentation: Segmentation):
@@ -112,3 +145,13 @@ def show_segmented_mesh(mesh: Polyhedron_3, segmentation: Segmentation):
             colors[i] = GREEN
     mp.plot(vertices, facets, c=colors)
 
+
+if __name__ == "__main__":
+    mesh = Polyhedron_3("output/surface_mesh.off")
+
+    segmentation = {}
+    for v in mesh.points():
+        segmentation[hash_point(v)] = bool(random.getrandbits(1))
+
+    mp.offline()
+    show_segmented_mesh(mesh, segmentation)
