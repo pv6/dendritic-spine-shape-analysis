078682e98fe325c7fcd752a01c02233946ffbcb3
Peter Vasilyev <p.vasilyev@inbox.ru>
2022-03-18 00:01:59 +0300

    Add mesh subdivision in chord distribution metric

--- spine_metrics.py
+++ spine_metrics.py
@@ -1,10 +1,12 @@
+import math
 from abc import ABC, abstractmethod
 from random import Random
 import numpy as np
 from typing import Any, List, Tuple, Dict
 import ipywidgets as widgets
-from CGAL.CGAL_Polyhedron_3 import Polyhedron_3, Polyhedron_3_Facet_handle
-from CGAL.CGAL_Polygon_mesh_processing import area, volume
+from CGAL.CGAL_Polyhedron_3 import Polyhedron_3, Polyhedron_3_Facet_handle, \
+    Polyhedron_3_Halfedge_handle, Polyhedron_3_Vertex_handle, Polyhedron_3_Edge_iterator
+from CGAL.CGAL_Polygon_mesh_processing import area, face_area, volume
 from CGAL.CGAL_Kernel import Ray_3, Point_3, Vector_3
 from CGAL.CGAL_AABB_tree import AABB_tree_Polyhedron_3_Facet_handle
 from spine_segmentation import point_2_list
@@ -90,7 +92,7 @@ def load_metrics(filename: str) -> Dict[str, List[SpineMetric]]:
                     value = np.fromstring(value_str[1:-1], dtype="float", sep=" ")
                 else:
                     value = float(value_str)
-                metrics.append(CustomSpineMetric(metric_name, value))
+                metrics.append(ManualSpineMetric(metric_name, value))
             output[spine_name] = metrics
     return output
 
@@ -150,11 +152,22 @@ class ChordDistributionSpineMetric(HistogramSpineMetric):
     num_of_chords: int
     chords: List[Tuple[Point_3, Point_3]]
     chord_lengths: List[float]
+    relative_max_facet_area: float
 
-    def __init__(self, spine_mesh: Polyhedron_3, num_of_chords: int = 1000, num_of_bins: int = 20) -> None:
+    def __init__(self, spine_mesh: Polyhedron_3, num_of_chords: int = 3000,
+                 num_of_bins: int = 100, relative_max_facet_area: float = 0.001) -> None:
         self.name = "Chord Distribution"
         self.num_of_chords = num_of_chords
+        self.relative_max_facet_area = relative_max_facet_area
         super().__init__(spine_mesh, num_of_bins)
+
+    @staticmethod
+    def _get_incident_halfedges(facet_halfedge: Polyhedron_3_Halfedge_handle) -> List[Polyhedron_3_Halfedge_handle]:
+        return [facet_halfedge, facet_halfedge.next(), facet_halfedge.next().next()]
+
+    @staticmethod
+    def _get_side_centers(facet_halfedge: Polyhedron_3_Halfedge_handle) -> List[Polyhedron_3_Halfedge_handle]:
+        return [facet_halfedge, facet_halfedge.next(), facet_halfedge.next().next()]
 
     @staticmethod
     def _calculate_facet_center(facet: Polyhedron_3_Facet_handle) -> Vector_3:
@@ -172,6 +185,18 @@ class ChordDistributionSpineMetric(HistogramSpineMetric):
         return center
 
     @staticmethod
+    def _is_triangle(facet: Polyhedron_3_Facet_handle) -> bool:
+        circulator = facet.facet_begin()
+        begin = facet.facet_begin()
+        i = 0
+        while circulator.hasNext():
+            i += 1
+            circulator.next()
+            if circulator == begin:
+                break
+        return i == 3
+
+    @staticmethod
     def _vec_2_point(vector: Vector_3) -> Point_3:
         return Point_3(vector.x(), vector.y(), vector.z())
 
@@ -179,18 +204,65 @@ class ChordDistributionSpineMetric(HistogramSpineMetric):
     def _point_2_vec(point: Point_3) -> Vector_3:
         return Vector_3(point.x(), point.y(), point.z())
 
+    def _subdivide_mesh(self, mesh: Polyhedron_3,
+                        relative_max_facet_area: float = 0.001) -> Polyhedron_3:
+        out: Polyhedron_3 = mesh.deepcopy()
+        total_area = area(out)
+        subdivision_number = 3
+
+        for i in range(subdivision_number):
+            # split every edge
+            center_vertices = set()
+            edges = [edge for edge in out.edges()]
+            for edge in edges:
+                first_half: Polyhedron_3_Halfedge_handle = out.split_edge(edge)
+                center_vertices.add(first_half.vertex())
+                a = self._point_2_vec(first_half.vertex().point())
+                b = self._point_2_vec(edge.vertex().point())
+                center = (a + b) / 2
+                first_half.vertex().set_point(self._vec_2_point(center))
+            # create center triangles
+            facets = [facet for facet in out.facets()]
+            for j, facet in enumerate(facets):
+                halfedge = facet.halfedge()
+                if halfedge.vertex() not in center_vertices:
+                    halfedge = halfedge.next()
+                centers = [halfedge, halfedge.next().next(),
+                           halfedge.next().next().next().next()]
+                new_side = out.split_facet(centers[0], centers[1])
+                new_side = out.split_facet(new_side, centers[2])
+                new_side = out.split_facet(new_side, centers[0])
+                new_side.facet().set_id(halfedge.facet().id())
+        return out
+
     def _calculate_raycast(self, ray_query: Ray_3,
                            tree: AABB_tree_Polyhedron_3_Facet_handle) -> None:
         intersections = []
         tree.all_intersections(ray_query, intersections)
 
         origin = self._point_2_vec(ray_query.source())
-        prev_dist = 0
 
         # sort intersections along the ray
         intersection_points = [self._calculate_facet_center(intersection.second)
                                for intersection in intersections]
         intersection_points.sort(key=lambda point: (point - origin).squared_length())
+
+        # remove doubles
+        i = 0
+        while i < len(intersection_points) - 1:
+            if (intersection_points[i] - intersection_points[i + 1]).squared_length() < 0.0000001:
+                del intersection_points[i]
+            else:
+                i += 1
+
+        # if len(intersection_points) % 2 != 0:
+        #     i = 0
+        #     while i < len(intersection_points) - 1:
+        #         if intersection_points[i] == intersection_points[i + 1]:
+        #             del intersection_points[i]
+        #         else:
+        #             i += 1
+        #     x = 30
 
         for j in range(1, len(intersection_points), 2):
             center_1 = intersection_points[j - 1]
@@ -203,8 +275,11 @@ class ChordDistributionSpineMetric(HistogramSpineMetric):
             #     continue
             # prev_dist = dist
 
-            self.chord_lengths.append(
-                np.sqrt((center_2 - center_1).squared_length()))
+            length = np.sqrt((center_2 - center_1).squared_length())
+            if length == 0:
+                j -= 1
+                continue
+            self.chord_lengths.append(length)
 
             self.chords.append(
                 (self._vec_2_point(center_1), self._vec_2_point(center_2)))
@@ -213,23 +288,33 @@ class ChordDistributionSpineMetric(HistogramSpineMetric):
         self.chord_lengths = []
         self.chords = []
 
-        tree = AABB_tree_Polyhedron_3_Facet_handle(spine_mesh.facets())
+        for i, facet in enumerate(spine_mesh.facets()):
+            facet.set_id(i)
+        subdivided_spine_mesh = self._subdivide_mesh(spine_mesh, self.relative_max_facet_area)
 
-        surface_points = [self._calculate_facet_center(facet) for facet in spine_mesh.facets()]
+        tree = AABB_tree_Polyhedron_3_Facet_handle(subdivided_spine_mesh.facets())
+
+        facets = [[] for i in range(spine_mesh.size_of_facets())]
+        for facet in subdivided_spine_mesh.facets():
+            facets[facet.id()].append(facet)
+
+        # surface_points = [self._calculate_facet_center(facet) for facet in facets]
 
         rand = Random()
         for i in range(self.num_of_chords):
-            ind1 = rand.randrange(0, len(surface_points))
-            ind2 = rand.randrange(0, len(surface_points))
+            ind1 = rand.randrange(0, len(facets))
+            ind2 = rand.randrange(0, len(facets))
             while ind1 == ind2:
-                ind2 = rand.randrange(0, len(surface_points))
-            p1 = surface_points[ind1]
-            p2 = surface_points[ind2]
+                ind2 = rand.randrange(0, len(facets))
+            f1 = facets[ind1][rand.randrange(0, len(facets[ind1]))]
+            f2 = facets[ind2][rand.randrange(0, len(facets[ind2]))]
+            p1 = self._calculate_facet_center(f1)
+            p2 = self._calculate_facet_center(f2)
             direction = p2 - p1
             direction.normalize()
 
-            ray_query = Ray_3(self._vec_2_point(p1 - direction * 500),
-                              self._vec_2_point(p2 + direction * 500))
+            ray_query = Ray_3(self._vec_2_point(p1 - direction * 5000),
+                              self._vec_2_point(p2 + direction * 5000))
             self._calculate_raycast(ray_query, tree)
 
         # # find bounding box
@@ -262,3 +347,37 @@ class ChordDistributionSpineMetric(HistogramSpineMetric):
         self.chord_lengths = np.asarray(self.chord_lengths) / max_len
 
         return self.chord_lengths
+
+
+class OldChordDistributionSpineMetric(ChordDistributionSpineMetric):
+    def __init__(self, spine_mesh: Polyhedron_3, num_of_chords: int = 3000,
+                 num_of_bins: int = 100, relative_max_facet_area: float = 0.001) -> None:
+        super().__init__(spine_mesh, num_of_chords, num_of_bins, relative_max_facet_area)
+        self.name = "Old Chord Distribution"
+
+    def _subdivide_mesh(self, mesh: Polyhedron_3,
+                        relative_max_facet_area: float = 0.001) -> Polyhedron_3:
+        out: Polyhedron_3 = mesh.deepcopy()
+        facets = [facet for facet in out.facets()]
+        total_area = area(out)
+
+        for facet in facets:
+            facet_area = face_area(facet, out)
+            relative_area = facet_area / total_area
+
+            # facet already small enough
+            if relative_area <= relative_max_facet_area:
+                continue
+
+            subdivision_number = int(np.ceil(math.log(relative_area / relative_max_facet_area, 3)))
+            triangles: List[Polyhedron_3_Halfedge_handle] = [facet.halfedge()]
+            for i in range(subdivision_number):
+                new_triangles = []
+                for halfedge in triangles:
+                    new_triangles.extend(self._get_incident_halfedges(halfedge))
+                    center = self._vec_2_point(self._calculate_facet_center(halfedge.facet()))
+                    new_v = out.create_center_vertex(halfedge).vertex()
+                    new_v.set_point(center)
+                triangles = new_triangles
+
+        return out
